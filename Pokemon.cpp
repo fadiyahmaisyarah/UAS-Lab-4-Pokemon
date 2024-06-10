c69d111d0650e14341fe0686a32f70d38396dff5
yaya <167690668+fadiyahmaisyarah@users.noreply.github.com>
2024-06-10 17:14:02 +0700

    Update Pokemon.cpp

--- Pokemon.cpp
+++ Pokemon.cpp
@@ -107,3 +107,97 @@ public:
     }
     
 };
+
+// Class Player
+class Player {
+public:
+    string name;
+    int level;
+    int coins = 0;
+    int expp = 0;
+    vector<Pokemon> ownedPokemons;
+
+    Player(string n, int l) : name(n), level(l) {}
+
+    void printInfo() {
+        cout << "Player: " << name << endl << "Level: " << level << endl;
+    }
+
+    void printOwnedPokemons() {
+        cout << "Pokemon yang dimiliki: " << endl;
+        for (int i = 0; i < ownedPokemons.size(); ++i) {
+            cout << i + 1 << ". " << ownedPokemons[i].name << "\n";
+        }
+    }
+
+    void addPokemon(const Pokemon &pokemon) {
+        ownedPokemons.push_back(pokemon);
+    }
+
+    void reward(int coinsReward, int expReward) {
+        coins += coinsReward;
+        level += expReward;
+        expp  += expReward;
+        cout << "Kamu mendapat " << coinsReward << " coins dan " << expReward << " exp!\n";
+    }
+
+    bool tryToCatch(Pokemon &pokemon) {
+        int roll = rand() % 100 + 1;
+        if (coins >= 10)
+        {   
+            coins -= 10;
+            cout << pokemon.name << " liar muncul!" << endl;
+            if (roll <= pokemon.catchRate) 
+            {
+                cout << "Selamat! Kamu berhasil menangkap " << pokemon.name << "!\n";
+                addPokemon(pokemon);
+                return true;
+            }   
+            else 
+            {
+                cout << "Oh tidak! " << pokemon.name << " melarikan diri!\n";
+                return false;
+            }
+        }
+        else 
+        {
+            cout << "Koin tidak cukup" << endl;
+            return false;
+        }
+    }
+};
+// Battle pokemon player dan musuh
+void battle(Pokemon &pokemon1, Pokemon &pokemon2, Player &player) {
+    srand(static_cast<unsigned int>(time(0)));
+
+    cout << "\nBattle dimulai: " << pokemon1.name << " vs " << pokemon2.name << "!\n\n";
+    while (!pokemon1.isFainted() && !pokemon2.isFainted()) {
+        int attackChoice;
+
+        // Giliran player
+        cout << "Giliran " << pokemon1.name << " Pilih attack : " << endl;
+        pokemon1.printAttacks();
+        cout << "Masukkan serangan pilihanmu di sini (1/2/3): ";
+        cin >> attackChoice;
+        pokemon1.attackPokemon(pokemon2, attackChoice - 1);
+        if (pokemon2.isFainted()) {
+            cout << pokemon2.name << " telah pingsan! " << pokemon1.name << " menang!\n";
+            player.reward(20, 1); // Hadiah koin dan exp dari battle
+            pokemon1.restoreHealth();
+            pokemon2.restoreHealth();
+            break;
+        }
+
+        // Giliran musuh (random)
+        attackChoice = rand() % pokemon2.attacks.size();
+        pokemon2.attackPokemon(pokemon1, attackChoice);
+        if (pokemon1.isFainted()) {
+            cout << pokemon1.name << " telah pingsan! " << pokemon2.name << " menang!\n";
+            pokemon1.restoreHealth();
+            pokemon2.restoreHealth();
+            break;
+        }
+
+        cout << pokemon1.name << " HP: " << pokemon1.hp << " | " << pokemon2.name << " HP: " << pokemon2.hp << "\n\n";
+    }
+}
