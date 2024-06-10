9ff9cd7eedf736172f27ab041985974179758ab3
dipwaaaa <168277757+dipwaaaa@users.noreply.github.com>
2024-06-10 21:32:50 +0700

    Pokemon.cpp

--- Pokemon.cpp
+++ Pokemon.cpp
@@ -201,3 +201,136 @@ void battle(Pokemon &pokemon1, Pokemon &pokemon2, Player &player) {
         cout << pokemon1.name << " HP: " << pokemon1.hp << " | " << pokemon2.name << " HP: " << pokemon2.hp << "\n\n";
     }
 }
+
+int main()
+{
+    srand(static_cast<unsigned int>(time(0)));
+
+    system("CLS");
+    string pokemon[] = {"Pikachu", "Charmander", "Bulbasaur", "Squirtle", "Mewtwo", "Snorlax", "Lucario", "Rayquaza", "Arceus", "Lugia"};
+    string pokemonuser, selectedpokemon, attack;
+    int HP, pilihanmenu, count, coins, coinsReward, expReward;
+    char name[50], yn;
+
+    vector<Attack> pikachuAttacks = {
+        Attack("Thunderbolt", 40),
+        Attack("Quick attack", 20),
+        Attack("Iron tail", 30)
+    };
+    vector<Attack> charmanderAttacks = {
+        Attack("Flamethrower", 40),
+        Attack("Scratch", 20),
+        Attack("Ember", 30)
+    };
+    vector<Attack> bulbasaurAttacks = {
+        Attack("Vine whip", 40),
+        Attack("Tackle", 20),
+        Attack("Bubble", 30)
+    };
+    vector<Attack> squirtleAttacks = {
+        Attack("Water gun", 40),
+        Attack("Tackle", 20),
+        Attack("Bubble", 30)
+    };
+    vector<Attack> mewtwoAttacks = {
+        Attack("Psystrike", 80),
+        Attack("Confusion", 40),
+        Attack("Psycho cut", 30)
+    };
+    
+    vector<Attack> snorlaxAttacks = {
+        Attack("Hyper beam", 50),
+        Attack("Tackle", 30),
+        Attack("Rest", 0)
+    };
+    
+    vector<Attack> lucarioAttacks = {
+        Attack("Dragon pulse", 50),
+        Attack("Bone rush", 30),
+        Attack("Quick attack", 20)
+    };
+    
+    vector<Attack> rayquazaAttacks = {
+        Attack("Dragon Ascent", 80),
+        Attack("Twister", 40),
+        Attack("Ice beam", 30)
+    };
+    
+    vector<Attack> arceusAttacks = {
+        Attack("Judgement", 80),
+        Attack("Earth power", 40),
+        Attack("Hyper voice", 30)
+    };
+    
+    vector<Attack> lugiaAttacks = {
+        Attack("Aeroblast", 80),
+        Attack("Hydro Pump", 40),
+        Attack("Whirlpool", 30)
+    };
+    vector<Pokemon> availablePokemons = {
+        Pokemon("Pikachu", 100, 10, pikachuAttacks, 50),
+        Pokemon("Charmander", 100, 10, charmanderAttacks, 30),
+        Pokemon("Bulbasaur", 100, 10, bulbasaurAttacks, 40),
+        Pokemon("Squirtle", 100, 10, squirtleAttacks, 35),
+        Pokemon("Mewtwo", 100, 10, mewtwoAttacks, 10),
+        Pokemon("Snorlax", 100, 10, snorlaxAttacks, 30),
+        Pokemon("Lucario", 100, 10, lucarioAttacks, 30),
+        Pokemon("Rayquaza", 100, 10, rayquazaAttacks, 10),
+        Pokemon("Arceus", 100, 10, arceusAttacks, 10),
+        Pokemon("Lugia", 100, 10, lugiaAttacks, 10)
+    };
+
+    cout << "Nama pemain: ";
+    cin >> name;
+    Player player(name, 1);
+    player.addPokemon(Pokemon("Pikachu", 100, 10, pikachuAttacks, 50));
+
+    mn: cout<<"\nMenu"<<endl;
+    cout<<"1. Info Player\n2. Inventory\n3. Pokedex\n4. Catch Pokemon (10 coins)\n5. Battle";
+    cout<<"\nMasukkan pilihanmu di sini (1/2/3/4/5): "; cin>>pilihanmenu;
+
+
+        switch (pilihanmenu) {
+            case 1:
+                player.printInfo();
+                break;
+
+            case 2:
+                cout << "\nKoin Anda: " << player.coins << endl;
+                cout << "EXP Anda: " << player.expp << endl;
+                break;
+
+            case 3: cout<<"\nPokemon mana yang ingin Anda lihat?"<<endl;
+            cout<<"1. Pikachu\n2. Charmander\n3. Bulbasaur\n4. Squirtle\n5. Mewtwo\n6. Snorlax\n7. Lucario\n8. Rayquaza\n9. Arceus\n10. Lugia";
+            cout<<"\nMasukkan pilihanmu di sini: "; cin>>pokemonuser;
+            
+            selectpokemon(pokemonuser, selectedpokemon, HP, attack);
+
+            cout<<"\nKamu telah memilih "<<selectedpokemon<<"! ("<<HP<<" HP)"<<endl;
+            cout<<"Attacks: "<<attack<<endl; break;
+
+            case 4: {
+                Pokemon &wildPokemon = availablePokemons[rand() % availablePokemons.size()];
+                player.tryToCatch(wildPokemon);
+                break;
+            }
+
+            case 5: count++;
+            player.printOwnedPokemons();
+            cout<<"Masukkan Pokemon pilihanmu di sini (1/2/3/4/5): ";
+            int index; cin>>index;
+            Pokemon &chosenPokemon = player.ownedPokemons[index - 1];
+            
+            srand(static_cast<unsigned int>(time(0)));
+            Pokemon &opponentPokemon = availablePokemons[rand() % availablePokemons.size()];
+            battle(chosenPokemon, opponentPokemon, player);
+            chosenPokemon.restoreHealth();
+            break;
+
+    }
+
+    cout<<"\nKembali ke menu? (Y/N): "; cin>>yn;
+    if ((yn=='Y') || (yn=='y')) {goto mn;}
+    else { cout<<"Terima kasih sudah bermain!"; }
+    return 0;
+}
